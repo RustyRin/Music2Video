ffe49115f920cb57177db71d7f238cac3fc02291
RustyRin <ggk494@gmail.com>
2018-09-17 19:20:47 -0400

    added some error messages and lowered memory usage just a little bit

--- music2video.py
+++ music2video.py
@@ -1,20 +1,21 @@
+import sys
+
 from moviepy.editor import *
 from moviepy.video.VideoClip import ImageClip
-from moviepy.audio.AudioClip import AudioArrayClip
 from PIL import Image, ImageFilter
 from mutagen.flac import FLAC
 from mutagen.easyid3 import EasyID3
 from mutagen.mp3 import MP3
-import os, sys
+import os
 sys.path.append(os.getcwd())
 import unicode_search
 
-# test
 
-make_4k = True      # do you want your video to be exported in 4K
+make_4k = True              # do you want your video to be exported in 4K. overwritten if debug mode is on
 make_whole_album = True     # do you want to make a video of the whole album
-debug_mode = False
-
+debug_mode = False          # makes the videos 5 seconds long and in 1080p, also does not upload
+upload = True              # do you want to upload. will not upload if debug mode is on. use for making the whole video but not uploading
+clear_export = True         # Do you want to clear the export folder when you start the program
 
 def number_or_files(dir):   # returns the number of files in folder
     return len([item for item in os.listdir(dir) if os.path.isfile(os.path.join(dir, item))])
@@ -85,6 +86,44 @@ def make_text(text, type, length=0, color='white', method='caption', align='nort
         return TextClip(text, size=size, color=color, font=font, method=method)
 
 
+def make_thumbnail(input_image, input_text, is_full_album=False):
+    temp = Image.open(input_image)
+    temp = temp.filter(ImageFilter.GaussianBlur(radius=50))
+    temp.save('working/background_blurred_thumb.png', 'PNG')
+    art_width, art_height = temp.size
+    temp.close()
+
+    art = ImageClip(input_image)
+    art = art.resize(width=542)
+    art = art.set_position((48, 88))
+
+    drop_image = Image.open('working/drop_square.png')
+    drop_image = drop_image.resize((art_width, art_height), Image.ANTIALIAS)
+    drop_image.save('working/drop_thumbnail.png')
+    drop_image.close()
+
+    drop_shadow = ImageClip('working/drop_thumbnail.png', transparent=True)
+    drop_shadow = drop_shadow.resize(width=600).set_position((35, 75)).set_opacity(0.75)
+
+    art_blurred = ImageClip('working/background_blurred_thumb.png')
+    art_blurred = art_blurred.set_position('center')
+    art_blurred = art_blurred.resize(width=1000).resize(2)
+
+    gradient = ImageClip('working/gradient.png')
+    gradient = gradient.resize(width=1280)
+
+    # drop_shadow = ImageClip('working/thumb_drop.png', transparent=True)
+
+    if is_full_album == False:
+        text = make_text(input_text, 'thumb')
+        text = text.set_position((640, 88))
+        return CompositeVideoClip([art_blurred, gradient, drop_shadow, art, text], size=(1280, 720))
+    else:
+        text = make_text(input_text + '\n(FULL ALBUM)', 'thumb')
+        text = text.set_position((640, 88))
+        return CompositeVideoClip([art_blurred, gradient, drop_shadow, art, text], size=(1280, 720))
+
+
 def clear_folder(folder):
     for the_file in os.listdir(folder):
         file_path = os.path.join(folder, the_file)
@@ -99,7 +138,8 @@ def clear_folder(folder):
 def main():
 
     # CLEARING EXPORT FOLDER
-    clear_folder('export')
+    if clear_export:
+        clear_folder('export')
 
     for filename in os.listdir('input/'):   # finds finds the cover art photo in the input folder
         if os.path.splitext(filename)[1] == '.png' or os.path.splitext(filename)[1] == '.jpg' or os.path.splitext(filename)[1] == '.JPEG':
@@ -129,7 +169,7 @@ def main():
     if make_4k:     # makes the album art ImageClip, resizes itm then positions it into place
         art_clip = art_clip.resize(width=1600).set_position((144, 300))
     else:
-        art_clip = art_clip.resize(width=800).set_postion((72, 150))
+        art_clip = art_clip.resize(width=800).set_position((72, 150))
 
     # GRADIENT CLIP, DROPSHADOW CLIP
     drop_image = Image.open('working/drop_square.png')
@@ -146,7 +186,8 @@ def main():
 
     totaltime = 0 # might not need, only see it in the make whole album part
 
-    for filename in os.listdir('input/'):
+    #for filename in os.listdir('input/'):
+    for filename in sorted(os.listdir('input/')):       # grabs the files in a SORTED order
 
         # just to make it pretty when i run it in
         print("________________________________________________________________________________________________________________________\n")
@@ -157,18 +198,72 @@ def main():
             # FLAC FILES
             if os.path.splitext(filename)[1] == '.flac':
                 audio = FLAC('input/' + str(filename))
-                song_number = "{0:0>3}".format(clean_tag(str(audio['tracknumber'])))
-                song_artist = clean_tag(str(audio['artist']))
-                song_track = clean_tag(str(audio['title']))
-                song_album = clean_tag(str(audio['album']))
+
+                try:        # Track number
+                    song_number = "{0:0>3}".format(clean_tag(str(audio['tracknumber'])))
+                except:
+                    print(filename + ': Track number not set or improper')
+                    return
+
+                try:        # Track artist
+                    song_artist = clean_tag(str(audio['artist']))
+                except:
+                    print(filename + ': Track artist not set or improper')
+                    return
+
+                try:        # Track title
+                    song_track = clean_tag(str(audio['title']))
+                except:
+                    print(filename + ': Track title not set or improper')
+                    return
+
+                try:        # Track album
+                    song_album = clean_tag(str(audio['album']))
+                except:
+                    print(filename + ': Track album not set or improper')
+                    return
+
+                try:        # Album artist
+                    album_artist = clean_tag(str(audio['albumartist']))
+                except:
+                    print(filename + ': Album artist not set or improper')
+                    return
+
                 vid_length = audio.info.length
             else:   # if it got past the first if then its either a FLAc or MP3 file, so else is good enough
-                audio = EasyID3('input\\' + str(filename))
-                song_number = "{0:0>3}".format(clean_tag(str(audio['tracknumber'])))
-                song_artist = clean_tag(str(audio['artist']))
-                song_track = clean_tag(str(audio['title']))
-                song_album = clean_tag(str(audio['album']))
-                audio = MP3('input\\' + str(filename))
+                audio = EasyID3('input/' + str(filename))
+
+                try:
+                    song_number = "{0:0>3}".format(clean_tag(str(audio['tracknumber'])))
+                    if '/' in song_number:
+                        raise Exception
+                except:
+                    print(filename + ': Track number not set or improper')
+                    return
+
+                try:
+                    song_artist = clean_tag(str(audio['artist']))
+                except:
+                    print(filename + ': Track artist not set or improper')
+                    return
+
+                try:
+                    song_track = clean_tag(str(audio['title']))
+                except:
+                    print(filename + ': Track title not set or improper')
+                    return
+
+                try:
+                    song_album = clean_tag(str(audio['album']))
+                except:
+                    print(filename + ': Track album not set or improper')
+
+                try:
+                    album_artist = clean_tag(str(audio['albumartist']))
+                except:
+                    print(filename + ': Album artist not set or improper')
+
+                audio = MP3('input/' + str(filename))
                 vid_length = audio.info.length
 
             if debug_mode:
@@ -177,6 +272,11 @@ def main():
             totaltime += vid_length
 
             print('Song: ' + song_track + '\nTrack number: ' + song_number + '\nArtist: ' + song_artist + '\nAlbum: ' + song_album + '\nAlbum Artist: ' + album_artist + '\n')
+
+            # MAKING THUMBNAIL
+            thumb = make_thumbnail(album_art, song_track)  # Uses the make thumb method then saves frame
+            thumb.save_frame('thumb/' + song_number + ".png")
+            del thumb
 
             # MAKING TEXT CLIPS
             if make_4k:
@@ -195,29 +295,55 @@ def main():
                 else:
                     clip_album = make_text(song_album, 'album').set_position((929, 225))
 
-                clip_track = make_text(song_track, 'track').set_position((1858, 800))
+                clip_track = make_text(song_track, 'track').set_position((929, 400))
 
             clip_audio = AudioFileClip('input/' + filename)
 
             # EXPORTING
             video = CompositeVideoClip([transparent, background_blurred, gradient, drop_shadow, art_clip, clip_artist, clip_album, clip_track]).set_audio(clip_audio).set_duration(vid_length)
             video.write_videofile(('export/' + song_number + '.mp4'), fps=1)    # it is a static image, the fps does not matter
+            if upload is True and debug_mode is False:
+                if sys.platform == 'linux':     # because the youtube_uploader binary is different for diffrent operating systems (lol no shit) gott do this since im trying to do linux
+                    os.system('./youtubeuploader_linux_amd64 -filename \"export/' + song_number + '.mp4\" -thumbnail \"thumb/' + song_number + '.png\" -title \"' + song_artist + ' // ' + song_track + '\" -metaJSON meta.json')
+                else:                           # for windows pleb
+                    os.system('youtubeuploader -filename \"export/' + song_number + '.mp4\" -thumbnail \"thumb/' + song_number + '.png\" -title \"' + song_artist + ' // ' + song_track + '\" -metaJSON meta.json')
 
             # CLOSING
-            clip_audio.close()
+            video.close()
             del video
+
+            clip_audio.close()
             del clip_audio
+
+            clip_track.close()
             del clip_track
+
+            clip_album.close()
             del clip_album
+
+            clip_artist.close()
             del clip_artist
 
-    # MAKING ALBUM VIDEO
-    if number_or_files('input/') > 2 and make_whole_album == True:
+            del audio
+
+            del song_number
+
+            del song_artist
+
+            del song_track
+
+            del vid_length
+
+    if number_or_files('input/') > 2 and make_whole_album is True:
         song_list = []  # this makes an array of all the video files in export. i found it easier to just re-export the video files in a cocatenate
+
+        thumb = make_thumbnail(album_art, song_album, True)
+        thumb.save_frame('thumb/album.png')
+        del thumb
 
         print("________________________________________________________________________________________________________________________\n")
 
-        for filename in os.listdir('export/'):
+        for filename in sorted(os.listdir('export/')):
             song_list.append(VideoFileClip('export/' + filename))
 
         whole_album = concatenate_videoclips(song_list, method='compose')
@@ -226,10 +352,17 @@ def main():
         else:
             whole_album = whole_album.resize(width=1920)
 
-        whole_album = whole_album.set_duration(totaltime)
-
+        whole_album = whole_album
         whole_album.write_videofile('export/album.mp4', fps=5)
 
+        if upload is True and debug_mode is False:
+            if sys.platform == "linux":         # for linux
+                os.system('./youtubeuploader_linux_amd64 -filename \"export/album.mp4\" -thumbnail \"thumb/album.png\" -title \"' + album_artist + ' // ' + song_album + ' (FULL ALBUM)\" -metaJSON meta-album.json')
+            else:                               # for windows
+                os.system('youtubeuploader -filename \"export/album.mp4\" -thumbnail \"thumb/album.png\" -title \"' + album_artist + ' // ' + song_album + ' (FULL ALBUM)\" -metaJSON meta-album.json')
+
+    print('!!! Done making and uploading videos !!!')
+    input()
 
 if __name__ == '__main__':
     main()
